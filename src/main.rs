15d1822a68b9376f120ce09dbdc2ef513effc201
Eric Nordelo <eric.nordelo39@gmail.com>
2022-05-30 16:06:21 +0200

    refactor: add lib module

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
+use minigrep::Config;
 use std::env;
-use std::fs;
 use std::process;
 
 fn main() {
@@ -13,26 +13,8 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let contents =
-        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
-
-    println!("With text:\n{}", contents);
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
     }
 }
